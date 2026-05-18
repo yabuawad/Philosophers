e575dc61d18ec0ea22fff48930b8cb7725743618
Yara Abuawadh <yabuawad@c2r3s1.42amman.com>
2026-05-18 15:29:53 +0300

    overtime sleep fixed aaa

--- philo/printers.c
+++ philo/printers.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   printers.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yabuawad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: yabuawad <yabuawad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 17:25:12 by yabuawad          #+#    #+#             */
-/*   Updated: 2026/05/14 17:25:13 by yabuawad         ###   ########.fr       */
+/*   Updated: 2026/05/18 15:29:32 by yabuawad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,22 @@ long	getrealtime(void)
 	return ((tv.tv_usec / 1000) + tv.tv_sec * 1000);
 }
 
-void	zzz(int time_to_sleep)
+void	zzz(int time_to_sleep, t_prop *prog)
 {
 	long	initial_time;
 
 	initial_time = getrealtime();
 	while ((getrealtime() - initial_time) < time_to_sleep)
+	{
+		pthread_mutex_lock(&prog->deathlock);
+		if (prog->death)
+		{
+			pthread_mutex_unlock(&prog->deathlock);
+			break ;
+		}
+		pthread_mutex_unlock(&prog->deathlock);
 		usleep(600);
+	}
 }
 
 void	myprint(t_prop *prop, int id, char *msg)
